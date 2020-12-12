55ea04357b54736954fbd83164eadbb55985817a
HaneeshaVakkalagadda <haneesha.v18@iiits.in>
2020-12-12 22:38:48 +0530

    changes

--- SOAD/soad/settings.py
+++ SOAD/soad/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,3 +136,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'orionwebsite123@gmail.com'
 EMAIL_HOST_PASSWORD = 'testing@321'
 EMAIL_PORT = 587
+
+django_heroku.settings(locals())
